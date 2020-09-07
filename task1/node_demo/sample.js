ac7ea7e239149f62efff56a8ae711505f18ce59d
saurabh2297 <saurabhgangwar621@gmail.com>
2020-09-07 15:28:52 +0530

    Create sample.js

---
+++ task1/node_demo/sample.js
@@ -0,0 +1,9 @@
+//creating demo file using node.js
+
+var fs = require('fs');
+fs.writeFile('demo.txt','this is node demo',function(err){
+    if (err){
+        return console.log(err);
+    }
+    console.log('file created');
+});
