535dc1e00bed4dc60ad3c74ba7cabae65937d6da
saurabh2297 <saurabhgangwar621@gmail.com>
2020-09-08 11:40:12 +0530

    Delete sample.js

--- task1/node_demo/sample.js
+++
@@ -1,9 +0,0 @@
-//creating demo file using node.js
-
-var fs = require('fs');
-fs.writeFile('demo.txt','this is node demo',function(err){
-    if (err){
-        return console.log(err);
-    }
-    console.log('file created');
-});
