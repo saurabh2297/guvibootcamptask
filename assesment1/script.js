7d54f8fa3c4fd381aa091cd814439f6553909c50
Saurabh Gangwar <saurabhgangwar621@gmail.com>
2020-10-10 20:11:09 +0530

    Update script.js

--- assesment1/script.js
+++ assesment1/script.js
@@ -3,7 +3,7 @@ document.getElementById("myForm").addEventListener("submit", check);
 function check(){
     console.log("1")
     for(let i=1;i<=8;i++){
-        let x = document.getElementById()
+        let x = document.getElementById(i)
         if(x.value == null){
             x.classList.add('error')
             alert("This field can't be left empty")
