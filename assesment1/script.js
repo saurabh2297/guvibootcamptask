a5024edb42f97465108bf391770b58c2cc5d267d
Saurabh Gangwar <saurabhgangwar621@gmail.com>
2020-10-10 19:37:08 +0530

    Add files via upload

---
+++ assesment1/script.js
@@ -0,0 +1,13 @@
+document.getElementById("myForm").addEventListener("submit", check);
+
+function check(){
+    console.log("1")
+    for(let i=1;i<=8;i++){
+        let x = document.getElementsById()
+        if(x.value == null){
+            x.classList.add('error')
+            alert("This field can't be left empty")
+        }
+    }
+    alert("form succcesfully submitted")
+}
