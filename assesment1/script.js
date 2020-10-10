19243f00b1b777c5d1d6ee0831c79fe94eeb36b0
Saurabh Gangwar <saurabhgangwar621@gmail.com>
2020-10-10 21:52:03 +0530

    Update script.js

--- assesment1/script.js
+++ assesment1/script.js
@@ -1,13 +1,39 @@
-document.getElementById("myForm").addEventListener("submit", check);
+let $form = document.querySelector("#myForm")
 
-function check(){
-    console.log("1")
+$form.addEventListener("submit",function(event){
+    event.preventDefault();
     for(let i=1;i<=8;i++){
         let x = document.getElementById(i)
-        if(x.value == null){
-            x.classList.add('error')
+        console.log(x.value)
+        if(!x.value){
+            x.classList.add("error")
             alert("This field can't be left empty")
+            return 0;
+        }
+        if(i== 8){
+            if(x.value>9999999999){
+                x.classList.add("error")
+                alert("Phone number can be of max 10 digits ")
+                return 0;
+            }
         }
     }
     alert("form succcesfully submitted")
-}
+});
+
+
+setInterval(function(){ 
+    
+    for(let i=1;i<=8;i++){
+        let x = document.getElementById(i)
+        if(i==8){
+            if(x.value<=9999999999){
+                x.classList.remove("error")
+            }
+        }
+        if(x.value){
+            if(i==8){continue;}
+            x.classList.remove("error")
+        }
+    }
+ }, 1000);
