001214a04c638b214747d05fdefff897ed51b8cd
Saurabh Gangwar <saurabhgangwar621@gmail.com>
2020-10-11 09:41:45 +0530

    Add files via upload

---
+++ week6/callback-hell/script.js
@@ -0,0 +1,39 @@
+$h1 = document.querySelector('#txt')
+
+setTimeout(() => {
+    $h1.innerHTML = "10"
+    setTimeout(() => {
+        $h1.innerHTML = "9"
+        setTimeout(() => {
+            $h1.innerHTML = "8"
+            setTimeout(() => {
+                $h1.innerHTML = "7"
+                setTimeout(() => {
+                    $h1.innerHTML = "6"
+                    setTimeout(() => {
+                        $h1.innerHTML = "5"
+                        setTimeout(() => {
+                            $h1.innerHTML = "4"
+                            setTimeout(() => {
+                                $h1.innerHTML = "3"
+                                setTimeout(() => {
+                                    $h1.innerHTML = "2"
+                                    setTimeout(() => {
+                                        $h1.innerHTML = "1"
+                                        setTimeout(() => {
+                                            $h1.innerHTML = "0"
+                                            setTimeout(() => {
+                                                $h1.innerHTML = "Happy Independence Day"
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);    
+                                }, 1000
+                                );
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
